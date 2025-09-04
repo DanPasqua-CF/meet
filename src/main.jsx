c896281ef8db3d16e43af1e6931f710a279747c4
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-09-04 17:41:35 -0400

    Add serviceWorker changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import * as atatus from 'atatus-spa';
-atatus.config('f99daba1486248f4a16a73a91c23a306').install();
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//import * as atatus from 'atatus-spa';
+//atatus.config('f99daba1486248f4a16a73a91c23a306').install();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+serviceWorkerRegistration.register();
