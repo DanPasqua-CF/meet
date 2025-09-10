11bea584bc69c007cb5404c94f0acb8e9012d508
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-09-10 12:21:35 -0400

    Add warning alert

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -14,8 +14,16 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert("");
+    }
+    else {
+      setWarningAlert("You are using the Meet application offline");
+    }
+
     fetchData();
   }, [currentCity, currentNumberOfEvents]);
 
@@ -32,6 +40,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch 
         allLocations={allLocations} 
